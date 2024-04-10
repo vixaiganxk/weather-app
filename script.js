a1a62ee39c9e6a62ca70f813d3653d6eb5dd3500
vixaiganxk <vijayganeshkaruppasamy@gmail.com>
2024-04-10 11:35:24 +0530

    Entire commit
    Committing all the changes and pushing it into the Github

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+const apiKey = "bb2e836ca143f24efbdfe352457f1e96";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+// Async function to check the weather
+async function checkWeather(city) {
+    try {
+        const response = await fetch(apiURL +city+ `&appid=${apiKey}`);
+        const data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Humidity") {
+            weatherIcon.src = "images/humidity.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
